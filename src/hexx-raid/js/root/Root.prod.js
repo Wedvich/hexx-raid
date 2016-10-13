014ae737b0f3d36666a209893899493187753e29
Martin Wedvich <wedvich@gmail.com>
2016-10-13 16:36:19 +0200

    Undid more dumb

--- src/hexx-raid/js/root/Root.prod.js
+++ src/hexx-raid/js/root/Root.prod.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import routes from './routes';
@@ -9,10 +9,14 @@ export default class Root extends Component {
       appInsights: this.props.appInsights
     };
   }
-  
+
   render() {
     return <Provider store={this.props.store}>
       <Router history={this.props.history} routes={routes} />
     </Provider>;
   }
 }
+
+Root.childContextTypes = {
+  appInsights: PropTypes.object
+};
